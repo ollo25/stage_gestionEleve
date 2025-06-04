0f2eceff2fea0d6bc6b4c09af0e16f1d71617ea4
Leo Omnes <l.omnes@lprs.fr>
2025-06-04 16:46:28 +0200

    new et index fait pour entreprise / potentiel eleve / etudiant

--- src/Form/EntrepriseForm.php
+++ src/Form/EntrepriseForm.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Entreprise;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,27 @@ class EntrepriseForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Nom')
-            ->add('Siret')
-            ->add('Adresse')
-        ;
+            ->add('Nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ex: Société XYZ'
+                ],
+                'label' => 'Nom de l\'entreprise'
+            ])
+            ->add('Siret', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => '123 456 789 00012'
+                ],
+                'label' => 'Numéro SIRET'
+            ])
+            ->add('Adresse', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => '123 Rue de Paris, 75000 Paris'
+                ],
+                'label' => 'Adresse complète'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/PotentielEleveForm.php
+++ src/Form/PotentielEleveForm.php
@@ -6,6 +6,9 @@ use App\Entity\PotentielEleve;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,18 +17,63 @@ class PotentielEleveForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Nom')
-            ->add('Prenom')
-            ->add('Email')
-            ->add('Telephone')
-            ->add('NumDossierPsup')
-            ->add('FiliereEnvisage')
-            ->add('AncienEtablissement')
+            ->add('Nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Dupont'
+                ],
+                'label' => 'Nom de famille'
+            ])
+            ->add('Prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Jean'
+                ],
+                'label' => 'Prénom'
+            ])
+            ->add('Email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'jean.dupont@example.com'
+                ]
+            ])
+            ->add('Telephone', TelType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => '0612345678'
+                ],
+                'label' => 'Téléphone'
+            ])
+            ->add('NumDossierPsup', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => '123456'
+                ],
+                'label' => 'Numéro dossier Parcoursup'
+            ])
+            ->add('FiliereEnvisage', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Informatique'
+                ],
+                'label' => 'Filière envisagée'
+            ])
+            ->add('AncienEtablissement', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Lycée Descartes'
+                ],
+                'label' => 'Ancien établissement'
+            ])
             ->add('refResponsable', EntityType::class, [
                 'class' => User::class,
-                'choice_label' => 'id',
-            ])
-        ;
+                'choice_label' => function(User $user) {
+                    return $user->getNom() . ' ' . $user->getPrenom();
+                },
+                'attr' => ['class' => 'form-select'],
+                'label' => 'Responsable',
+                'placeholder' => 'Sélectionnez un responsable'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
